fbca8992e8eca6d30eecfad718b8e74f628512c3
JANG HYO JE <113086153+Dwawawawa@users.noreply.github.com>
2026-01-09 03:21:17 +0900

    FAIL: 내가 졸려서 제정신이 아니라 효제씬 못고침 현준씬에서 끝냄

--- Client/main.cpp
+++ Client/main.cpp
@@ -27,7 +27,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	windowManager.Initialize(L"Aurora");
 
 	SceneManager& sceneManager = SceneManager::GetInstance();
-	sceneManager.ChangeScene("HyojeTestScene");
+	sceneManager.ChangeScene("ClientTestScene");
 
 	while (windowManager.ProcessMessages()) sceneManager.Run();
 
